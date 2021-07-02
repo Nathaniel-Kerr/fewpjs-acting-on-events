6d86f8bf5cb55bd6c8c85156c9d24db8f476ab5d
Nathaniel <NathanielKerr10@gmail.com>
2021-07-01 17:25:24 -0700

    commit tree 5eeb891b96659c841fc25d4ef93f8d38055d78bc

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+
+console.log(dodger.style.left);
+console.log(dodger.style.bottom);
+
+// dodger.style.bottom = "100px";
+dodger.style.bottom = "0px"
