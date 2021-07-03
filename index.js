12babb7000c6e36c3805c80cb626a6eb11b89fa0
Nathaniel <NathanielKerr10@gmail.com>
2021-07-03 13:41:50 -0700

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var dodger = document.getElementById('dodger');
+let dodger = document.getElementById('dodger');
 
 dodger.style.backgroundColor = "#FF69B4";
 
@@ -11,8 +11,8 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  var leftNumbers = dodger.style.left.replace('px', '');
-  var left = parseInt(leftNumbers, 10)
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10)
   if (left > 0) {
     dodger.style.left = `${left + 1}px`
   }
